5ae0de54810a68c132f7e5d00f5ee68e256b8bf8
Shahzad Ahmad <shahzad3766952@outlook.com>
2019-10-25 18:40:47 +0500

    some code

--- Models/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("server_name", "localhost");
-define("user","root");
-define("pass","");
-define("database","aroma");
-$connect = mysqli_connect(server_name, user, pass, database) or die("failed to connect to database");
-
-?>
