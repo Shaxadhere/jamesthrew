d7755e3b84178152b3e8a5dd66ab417459a81547
umairhere <umairkhan515yusafzai@gmail.com>
2019-10-30 03:23:53 -0700

    bb

--- admin/faqs/index.php
+++ admin/faqs/index.php
@@ -1,7 +1,10 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT'].'/jamesthrew/appdata/WebConfig.php');
+include($root.'/admin/faqs/model.php');
 $pageName = "FAQs";
-getHeader($pageName, $root."/shared/adminheader.php")
+getHeader($pageName, $root."/shared/adminheader.php");
+$listing = new faqsModel();
+$list = $listing->fetch();
 ?>
 
 <nav class="page-breadcrumb">
@@ -10,6 +13,74 @@ getHeader($pageName, $root."/shared/adminheader.php")
 		<li class="breadcrumb-item active" aria-current="page"><?php echo $pageName ?></li>
 	</ol>
 </nav>
+
+<div id="contentt">
+<div class="row">
+	<div class="col-md-12 grid-margin stretch-card">
+    	<div class="card">
+        	<div class="card-body">
+			<a href="#" id="det" class="btn btn-primary" data-target="create">Add New</a>
+			<br>
+			<br>
+            	<h6 class="card-title">All FAQS</h6>
+                <div class="table-responsive">
+                  <table id="dataTableExample" class="table">
+                    <thead>
+                      <tr>
+                        <th>Quations</th>
+                        <th>Answers</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody id="mon">
+					<?php
+
+					while($arr = mysqli_fetch_array($list))
+					{
+						echo "
+						<tr>
+                        	<td>$arr[Question]</td>
+                        	<td>$arr[Answer]</td>
+                        	<td><a href='#' id='det' class='btn btn-primary' data-href='$arr[PK_ID]' data-target='create?$arr[PK_ID]'>View Details</a></td>
+                      	</tr>
+						";
+					}
+					?>
+                      
+                    </tbody>
+                  </table>
+                </div>
+            </div>
+        </div>
+	</div>
+</div>
+<?php
+$create = new faqsModel();
+if(isset($_POST['Create'])){
+	$Question = $_POST['Question'];
+	$Answer  = $_POST['Answer'];
+	
+	$res = $create->AddNew($Question, $Answer);
+	if($res){
+		redirectWindow('index');
+	}
+	else{
+		showAlert("Something went wrong");
+	}
+}
+if(isset($_POST['Save'])){
+	$Question = $_POST['Question'];
+	$Answer = $_POST['Answer'];
+
+}
+?>
+
+
+
+
+
+
+
 
 <?php
 getFooter($root.'/shared/adminfooter.php');
